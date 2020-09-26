7c26f05904d5e9fa4c0e95d1c6cdad4525dd1350
varkhol.yuliia <juljailchenko@gmail.com>
2020-09-26 23:00:28 +0300

    Updates

---
+++ precache-manifest.fe964af4c5e0a1dd4401030b89aa7bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684cbaf5b93696a089d058443b0c0a70",
+    "url": "/react-class-components/index.html"
+  },
+  {
+    "revision": "ff6f59331ecda7cdb6b6",
+    "url": "/react-class-components/static/css/main.40149c8a.chunk.css"
+  },
+  {
+    "revision": "ff5247850d0e39b6b7e5",
+    "url": "/react-class-components/static/js/2.69ce7b26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-class-components/static/js/2.69ce7b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6f59331ecda7cdb6b6",
+    "url": "/react-class-components/static/js/main.cc1ffa8b.chunk.js"
+  },
+  {
+    "revision": "006ef9aaf677474a3c98",
+    "url": "/react-class-components/static/js/runtime-main.cf0d1964.js"
+  },
+  {
+    "revision": "73d0609a4b976a0015b47ace4fac1e8e",
+    "url": "/react-class-components/static/media/loupe.73d0609a.svg"
+  }
+]);
